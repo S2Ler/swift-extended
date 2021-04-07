56dcf70283f1e8b87eeee284ef689d9e14cb6d66
Aliaksandr Bialiauski <aliaksandr@bialiauski.com>
2021-04-07 09:56:11 +0300

    AttributedStringBuilder: New init, new add attributes method

--- Sources/SwiftExtended/UI/AttributedStringBuilder.swift
+++ Sources/SwiftExtended/UI/AttributedStringBuilder.swift
@@ -3,103 +3,124 @@ import UIKit
 
 /// Allows creating NSAttributedStrings with builder like API.
 public final class AttributedStringBuilder {
-    private var storage = NSMutableAttributedString()
+  private var storage = NSMutableAttributedString()
 
-    public init() {}
+  public init() {}
 
-    /// Resets builder state
-    @discardableResult
-    public func reset() -> AttributedStringBuilder {
-        storage = NSMutableAttributedString()
-        return self
+  /// Initialise builder with initial string.
+  public init(_ initialString: NSMutableAttributedString) {
+    self.storage = initialString
+  }
+
+  /// Resets builder state
+  @discardableResult
+  public func reset() -> AttributedStringBuilder {
+    storage = NSMutableAttributedString()
+    return self
+  }
+
+  /// Makes copy of current attributedString.
+  public func build() -> NSAttributedString {
+    makeCurrentStorageCopy()
+  }
+
+  @discardableResult
+  public func append(_ string: String,
+                     font: UIFont? = nil,
+                     color: UIColor? = nil,
+                     letterSpacing: CGFloat? = nil,
+                     adjustParagraph: ((NSMutableParagraphStyle) -> Void)? = nil,
+                     adjustAttributes: ((inout [NSAttributedString.Key: Any]) -> Void)? = nil) -> Self {
+    var attributes: [NSAttributedString.Key: Any] = [:]
+
+    if let font = font {
+      attributes[.font] = font
     }
 
-    /// Makes copy of current attributedString.
-    public func build() -> NSAttributedString {
-        return storage.copy() as! NSAttributedString
+    if let color = color {
+      attributes[.foregroundColor] = color
     }
 
-    @discardableResult
-    public func append(_ string: String,
-                       font: UIFont? = nil,
-                       color: UIColor? = nil,
-                       letterSpacing: CGFloat? = nil,
-                       adjustParagraph: ((NSMutableParagraphStyle) -> Void)? = nil,
-                       adjustAttributes: ((inout [NSAttributedString.Key: Any]) -> Void)? = nil) -> Self {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-
-        if let font = font {
-            attributes[.font] = font
-        }
-
-        if let color = color {
-            attributes[.foregroundColor] = color
-        }
-
-        if let letterSpacing = letterSpacing {
-            attributes[.kern] = letterSpacing
-        }
-
-        if let adjustParagraph = adjustParagraph {
-            let paragraph = NSMutableParagraphStyle()
-            adjustParagraph(paragraph)
-            attributes[.paragraphStyle] = paragraph
-        }
-
-        adjustAttributes?(&attributes)
-
-        return append(NSAttributedString(string: string, attributes: attributes))
+    if let letterSpacing = letterSpacing {
+      attributes[.kern] = letterSpacing
     }
 
-    @discardableResult
-    public func append(_ attributedString: NSAttributedString) -> Self {
-        self.storage.append(attributedString)
-        return self
+    if let adjustParagraph = adjustParagraph {
+      let paragraph = NSMutableParagraphStyle()
+      adjustParagraph(paragraph)
+      attributes[.paragraphStyle] = paragraph
     }
 
-    /// Appends `image` and move it by `verticalDisposition` points down
-    @discardableResult
-    public func append(image: UIImage, tintColor: UIColor? = nil, verticalDisposition: CGFloat = 0) -> Self {
-        let iconAttachment = NSTextAttachment()
-        iconAttachment.bounds = CGRect(x: 0, y: verticalDisposition,
-                                       width: image.size.width, height: image.size.height)
-        iconAttachment.image = image
-        let iconString = NSMutableAttributedString(attachment: iconAttachment)
-        if let tintColor = tintColor {
-            if #available(iOS 13, *) {
-                iconString.addAttributes([.foregroundColor: tintColor], range: NSRange(location: 0, length: iconString.length))
-            } else {
-                // On <iOS13 we need to add a character with a foreground color before the image,
-                // in order for the image to get a color.
-                // Reference: https://stackoverflow.com/questions/29041458/how-to-set-color-of-templated-image-in-nstextattachment
-                append("\0", color: tintColor)
-            }
-        }
-        return append(iconString)
-    }
+    adjustAttributes?(&attributes)
 
-    /// Breaks current line
-    @discardableResult
-    public func newLine(count: Int = 1) -> Self {
-        return append(String(repeating: "\n", count: count))
-    }
+    return append(NSAttributedString(string: string, attributes: attributes))
+  }
 
-    /// Breaks current line and insert an empty line
-    @discardableResult
-    public func emptyLine() -> Self {
-        return append("\n\n")
-    }
+  @discardableResult
+  public func append(_ attributedString: NSAttributedString) -> Self {
+    self.storage.append(attributedString)
+    return self
+  }
 
-    /// Adds `count` empty space chars
-    @discardableResult
-    public func space(count: Int = 1) -> Self {
-        return append(String(repeating: " ", count: count))
+  /// Appends `image` and move it by `verticalDisposition` points down
+  @discardableResult
+  public func append(image: UIImage, tintColor: UIColor? = nil, verticalDisposition: CGFloat = 0) -> Self {
+    let iconAttachment = NSTextAttachment()
+    iconAttachment.bounds = CGRect(x: 0, y: verticalDisposition,
+                                   width: image.size.width, height: image.size.height)
+    iconAttachment.image = image
+    let iconString = NSMutableAttributedString(attachment: iconAttachment)
+    if let tintColor = tintColor {
+      if #available(iOS 13, *) {
+        iconString.addAttributes([.foregroundColor: tintColor], range: NSRange(location: 0, length: iconString.length))
+      } else {
+        // On <iOS13 we need to add a character with a foreground color before the image,
+        // in order for the image to get a color.
+        // Reference: https://stackoverflow.com/questions/29041458/how-to-set-color-of-templated-image-in-nstextattachment
+        append("\0", color: tintColor)
+      }
     }
+    return append(iconString)
+  }
 
-    /// Applies `attributes` for chars in `range`
-    @discardableResult
-    public func addAttributes(_ attributes: [NSAttributedString.Key: Any], range: NSRange) -> Self {
-        storage.addAttributes(attributes, range: range)
-        return self
-    }
+  /// Breaks current line
+  @discardableResult
+  public func newLine(count: Int = 1) -> Self {
+    return append(String(repeating: "\n", count: count))
+  }
+
+  /// Breaks current line and insert an empty line
+  @discardableResult
+  public func emptyLine() -> Self {
+    return append("\n\n")
+  }
+
+  /// Adds `count` empty space chars
+  @discardableResult
+  public func space(count: Int = 1) -> Self {
+    return append(String(repeating: " ", count: count))
+  }
+
+  /// Applies `attributes` for chars in `range`
+  @discardableResult
+  public func addAttributes(_ attributes: [NSAttributedString.Key: Any], range: NSRange) -> Self {
+    storage.addAttributes(attributes, range: range)
+    return self
+  }
+
+  /// Applies `attributes` for chars in range which is obtained from `rangeBlock`.
+  /// - Parameters:
+  ///   - rangeBlock: A block which should return NSRange to apply `attributes` to.
+  ///                 The current attributed string is passed to this block.
+  @discardableResult
+  public func addAttributes(_ attributes: [NSAttributedString.Key: Any],
+                            rangeBlock: (NSAttributedString) -> NSRange) -> Self {
+    let range = rangeBlock(makeCurrentStorageCopy())
+    storage.addAttributes(attributes, range: range)
+    return self
+  }
+
+  private func makeCurrentStorageCopy() -> NSAttributedString {
+    storage.copy() as! NSAttributedString
+  }
 }
